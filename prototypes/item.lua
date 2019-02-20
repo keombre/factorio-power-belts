75191386b54837f0fd19fce4503c12ecc50b2057
keombre <keombre8@gmail.com>
2019-02-21 00:45:34 +0100

    finish pole entity

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -4,12 +4,18 @@ local fast_transport_belt_power    = table.deepcopy(data.raw.item["fast-transpor
 local express_transport_belt_power = table.deepcopy(data.raw.item["express-transport-belt"])
 
 transport_belt_power.name          = "transport-belt-power"
+transport_belt_power.icon          = "__power-belts__/graphics/transport-belt.png"
+transport_belt_power.order         = "a[transport-belt]-x[power-belts]"
 transport_belt_power.place_result  = "transport-belt-power"
 
 fast_transport_belt_power.name         = "fast-transport-belt-power"
+fast_transport_belt_power.icon         = "__power-belts__/graphics/fast-transport-belt.png"
+fast_transport_belt_power.order        = "a[transport-belt]-y[power-belts]"
 fast_transport_belt_power.place_result = "fast-transport-belt-power"
 
 express_transport_belt_power.name         = "express-transport-belt-power"
+express_transport_belt_power.icon         = "__power-belts__/graphics/express-transport-belt.png"
+express_transport_belt_power.order        = "a[transport-belt]-z[power-belts]"
 express_transport_belt_power.place_result = "express-transport-belt-power"
 
 data:extend{
--- prototypes/pole.lua
+++ prototypes/pole.lua
@@ -12,34 +12,30 @@ local pole = {
         "hide-from-bonus-gui",
         "hide-alt-info"
     },
-
-    maximum_wire_distance = 9,
-    supply_area_distance = 3.5,
+    selectable_in_game = false,
+    mined_sound = nil,
+    minable = nil,
+    collision_box = nil,
+    selection_box = nil,
+    collision_mask = {},
+    vehicle_impact_sound = nil,
+    maximum_wire_distance = 1,
+    supply_area_distance = 1,
     pictures =
     {
-      filename = "__base__/graphics/entity/medium-electric-pole/medium-electric-pole.png",
+      filename = "__power-belts__/graphics/pole.png",
       priority = "high",
-      width = 136,
-      height = 122,
+      width = 18,
+      height = 17,
       direction_count = 1,
-      shift = {1.4, -1.0}
+      shift = {0.4, 0.42}
     },
     connection_points =
     {
-      {
-        shadow =
         {
-          copper = {2.55, 0.4},
-          green = {2.0, 0.4},
-          red = {3.05, 0.4}
-        },
-        wire =
-        {
-          copper = {-0.03125, -2.46875},
-          green = {-0.34375, -2.46875},
-          red = {0.25, -2.46875}
+            shadow = {copper = {0.37, 0.42}},
+            wire = {copper = {0.38, 0.31}}
         }
-      }
     },
     radius_visualisation_picture =
     {
